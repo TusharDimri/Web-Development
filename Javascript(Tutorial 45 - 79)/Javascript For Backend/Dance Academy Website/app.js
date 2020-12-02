2e380e0465cf9036fe550b2f2f43445424ca2d19
Tushar <tushar.dimri22@gmail.com>
2020-12-02 20:02:15 +0530

    Commit

--- Javascript(Tutorial 45 - 79)/Javascript For Backend/Dance Academy Website/app.js
+++ Javascript(Tutorial 45 - 79)/Javascript For Backend/Dance Academy Website/app.js
@@ -2,8 +2,11 @@ const { urlencoded } = require('express');
 const express = require('express');
 const path = require('path');
 
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/contactDance', {useNewUrlParser : true});
+
 const app = express();
-const port = 8000;
+const port = 80;
 
 app.use("/static", express.static('static')); 
 
@@ -17,8 +20,29 @@ app.get("/", (request, response)=>{
     response.status(200).render('home.pug');
 });
 
+// Defining Mongoose Schema:-
+var contactSchema = new mongoose.Schema({
+    name : String,
+    phone : Number,
+    email : String,
+    address : String,
+    desc : String,
+});
+// Compliling Schema to a Model:-
+var Contact = mongoose.model('Contact', contactSchema);
+
 app.get("/contact", (request, response)=>{
     response.status(200).render('contact.pug');
+});
+
+app.post("/contact", (request, response)=> {
+    var myData = new Contact(request.body);
+    myData.save().then(()=>{
+        response.status(400).render("contact.pug");
+    }).catch(()=>{
+        response.send("Sorry! We coudnn't save yor contact to the database");
+    });
+    // response.status(200).render('contact.pug');
 });
 
 app.listen(port, ()=>{
