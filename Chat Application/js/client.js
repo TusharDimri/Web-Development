ac3790228ef1074d3deb6f97e962e17e567dbbb3
Tushar <tushar.dimri22@gmail.com>
2020-12-26 19:47:38 +0530

    Commit Message

--- Chat Application/js/client.js
+++ Chat Application/js/client.js
@@ -1,8 +1,9 @@
 const socket = io('http://localhost:8000');
+// Above line gives us the socket instance of the connection with client and server(socket.io server).
 
+const messageContainer = document.querySelector('.container');
 const form = document.getElementById('sendContainer');
 const messageInput = document.getElementById('messageInp');
-const messageContainer = document.querySelector('.container');
 var audio = new Audio('ting.mp3');
 
 const append = (message, position) => {
@@ -16,14 +17,16 @@ const append = (message, position) => {
     }
 };
 
-// const name = prompt("Enter your name to join");
-name = "none";
+const name = prompt("Enter your name to join");
 socket.emit('new-user-joined', name);
+// Above line emits the new-user-joined evnt to the server.
 
+// Here, we listen to the user-joined event emitted by the server:-
 socket.on('user-joined', name=>{
     append(`<b>${name}</b> joined the chat`, 'left');
 });
 
+// Listening to the submit emit fired when user submits the message:-
 form.addEventListener('submit', e => {
     e.preventDefault();
     const message = messageInput.value;
@@ -32,10 +35,12 @@ form.addEventListener('submit', e => {
     messageInput.value =   ``;
 });
 
+// Listening to the recive event emitted by the user and use the message recived by the server(from ohter client) and sent to the client
 socket.on('receive', data => {
     append(`<b>${data.name}</b>: ${data.message}`, 'left');
 });
 
+// Listen to leave event wmitted by the server:-
 socket.on('leave', name=>{
     append(`<b>${name}</b> left the chat`, 'left');
 })
