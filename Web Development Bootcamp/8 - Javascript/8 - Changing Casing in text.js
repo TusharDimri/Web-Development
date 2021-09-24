5318105a353efc10dec7fccfb7e6cd2341a454a1
Tushar <tushar.dimri22@gmail.com>
2021-09-24 14:01:28 +0530

    Commit

--- Web Development Bootcamp/8 - Javascript/8 - Changing Casing in text.js
+++ Web Development Bootcamp/8 - Javascript/8 - Changing Casing in text.js
@@ -1,7 +1,7 @@
-var nm = "Tushar";
+// var nm = "Tushar";
 
-console.log(nm.toUpperCase());
-console.log(nm.toLowerCase());
+// console.log(nm.toUpperCase());
+// console.log(nm.toLowerCase());
 
 var input = prompt("Enter your name");
-console.log("Hii", input.slice(0, 1).toUpperCase() + input.slice(1));
+console.log("Hii", input.slice(0, 1).toUpperCase() + input.slice(1).toLowerCase());
