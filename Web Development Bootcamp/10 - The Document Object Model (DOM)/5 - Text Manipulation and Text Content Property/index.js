a2c3458684a7ad1ce03e1331abf4dcb25e453b8f
Tushar <tushar.dimri22@gmail.com>
2021-11-26 20:17:31 +0530

    Commit

---
+++ Web Development Bootcamp/10 - The Document Object Model (DOM)/5 - Text Manipulation and Text Content Property/index.js
@@ -0,0 +1,5 @@
+document.querySelector("h1").innerHTML = `<i>Hello World</i>`;
+console.log(document.querySelector("h1").innerHTML);
+
+document.querySelector("h1").textContent = "Goodbye";
+console.log(document.querySelector("h1").textContent);
---
+++ Web Development Bootcamp/10 - The Document Object Model (DOM)/6 - Manipulating HTML Element Attributes/index.js
@@ -0,0 +1,6 @@
+// Attributes of an HTML tag are: class, id, href, src, etc.
+
+console.log(document.querySelector('a').attributes)
+console.log(document.querySelector('a').getAttribute("href"));
+document.querySelector("a").innerText = "Code With Harry";
+document.querySelector('a').setAttribute("href", "https://codewithharry.com");
