b211af8e14353bac91c6d5f142b8f102acd7be3b
Tushar <tushar.dimri22@gmail.com>
2021-11-29 11:03:46 +0530

    Commit Message

--- Web Development Bootcamp/12 - Advanced Javascript and DOM Manipulation/Drum Kit/index.js
+++ Web Development Bootcamp/12 - Advanced Javascript and DOM Manipulation/Drum Kit/index.js
@@ -1,3 +1,48 @@
-function handleClick() {  
-    alert("I got clicked."); 
-}
+let crash = new Audio("sounds/crash.mp3");
+let kick_bass = new Audio("sounds/kick-bass.mp3");
+let snare = new Audio("sounds/snare.mp3");
+let tom_1 = new Audio("sounds/tom-1.mp3");
+let tom_2 = new Audio("sounds/tom-2.mp3");
+let tom_3 = new Audio("sounds/tom-3.mp3");
+let tom_4 = new Audio("sounds/tom-4.mp3");
+
+document.querySelectorAll('button').forEach( function (item) {
+    item.addEventListener('click', function () {
+        // console.log(this);
+        // console.log(this.innerHTML);
+        // this.classList.toggle('color-white');
+        buttonInnerHTML = this.innerHTML;
+        switch (buttonInnerHTML) {
+            case 'w':
+                tom_1.play();
+                break;
+            
+            case 'a':
+                tom_2.play();
+                break;
+            
+            case 's':
+                tom_3.play();
+                break;
+            
+            case 'd':
+                tom_4.play();
+                break;
+            
+            case 'j':
+                snare.play();
+                break;
+            
+            case 'k':
+                kick_bass.play();
+                break;
+            
+            case 'l':
+                crash.play();
+                break;            
+            
+            default:
+                break;
+        }
+    })
+});
