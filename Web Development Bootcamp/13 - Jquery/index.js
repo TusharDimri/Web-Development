8893fe145134ac09dbccb15d4f394ba00638f1f0
Tushar <tushar.dimri22@gmail.com>
2022-01-14 21:46:34 +0530

    Commit

--- Web Development Bootcamp/13 - Jquery/index.js
+++ Web Development Bootcamp/13 - Jquery/index.js
@@ -1,4 +1,4 @@
-// jQuery or $ mean the same thing - document.querySelector();
+// jQuery or $ mean the same thing i.e. document.querySelector() and document.querySelectorAll();
 
 // .ready() method is required when we have the script tags for our Javascript and jQuery both at the head of the page.
 // $(document).ready(function () {
@@ -8,3 +8,19 @@
 // $("h1").css("color", "blue");
 
 // It's better to incorporate your Javascript and Jquery before the end of the body tags.
+
+
+// 1.  Selecting Elements using Jquery:
+// Using Javascript:
+console.log(document.querySelector("h1"));
+// Using Jquery:
+console.log($("h1"));
+console.log($("button"));
+
+
+// 2. Manipulating CSS styles using Jquery:
+// Using Javascript:
+document.querySelector("h1").style.color = "blue";
+// Using Jquery:
+$("h1").css("color", "blue")
+$("button").css("padding", "10px");
