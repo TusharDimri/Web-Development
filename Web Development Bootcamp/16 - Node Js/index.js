da341e3958729b85097c9b6d5a610500a01c3597
Tushar <tushar.dimri22@gmail.com>
2022-02-01 22:30:45 +0530

    Commit Message

---
+++ Web Development Bootcamp/16 - Node Js/index.js
@@ -0,0 +1,11 @@
+console.log("Hey, I'm writing Javascript in my system with Node Js.");
+
+// const fs = require("fs");
+
+// fs.copyFileSync("testfile.txt", "copiedfile.txt");
+
+const superheroes = require("superheroes");
+
+let hero_name = superheroes.random()
+
+console.log(hero_name);
