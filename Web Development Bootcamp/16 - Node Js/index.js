b15263226a712e6aa47579ddc34d87692ac985f8
Tushar <tushar.dimri22@gmail.com>
2022-02-01 22:33:34 +0530

    Commit

--- Web Development Bootcamp/16 - Node Js/index.js
+++ Web Development Bootcamp/16 - Node Js/index.js
@@ -5,7 +5,10 @@ console.log("Hey, I'm writing Javascript in my system with Node Js.");
 // fs.copyFileSync("testfile.txt", "copiedfile.txt");
 
 const superheroes = require("superheroes");
-
-let hero_name = superheroes.random()
+let hero_name = superheroes.random();
 
 console.log(hero_name);
+
+const supervillians = require("supervillains");
+let villian_name = supervillians.random();
+console.log(villian_name);
